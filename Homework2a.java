715332188be2768672235e6e55335fc97819ba70
Jing Xu <jing.xu@globoforce.com>
2018-01-08 13:38:54 +0000

    Homework2a

---
+++ Homework2a.java
@@ -0,0 +1,7 @@
+public class Homework2a {
+
+    public static void main (String[] args)
+    {
+        System.out.print("We are work HUMAN. \nUnlock ful potential. Celebrate postive moments.\nEveryday.");
+    }
+}
